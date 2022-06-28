d1bff5ca6dab3cb2e6205a9abd68477d2ade8d55
Joao Alba <joaop.alba@gmail.com>
2022-06-27 23:28:36 -0300

    Criado archetype inicial com dependências básicas.

---
+++ src/main/java/br/com/serasascore/SerasaScoreApplication.java
@@ -0,0 +1,13 @@
+package br.com.serasascore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SerasaScoreApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SerasaScoreApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/serasascore/SerasaScoreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.serasascore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SerasaScoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
